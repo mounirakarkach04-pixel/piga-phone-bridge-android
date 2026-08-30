cc352e3cf8ba57be343431d19b9ba6896d608724
mounirakarkach04-pixel <mounirakarkach04@gmail.com>
2026-08-31 00:06:23 +0200

    feat(pocket-shell): add tokenless Android build config

---
+++ pocket-shell/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pigapocket.enterprise.shell"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.pigapocket.enterprise"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0.1"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isDebuggable = true
+        }
+        getByName("release") {
+            isDebuggable = false
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
