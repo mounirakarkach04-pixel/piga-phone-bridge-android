ede8bca0f93c7acbc423e7cf3f3a5f98969a4516
mounirakarkach04-pixel <mounirakarkach04@gmail.com>
2026-09-02 19:35:48 +0200

    build(kalnbach): configure standalone APK

---
+++ kalnbach-app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "de.kalnbach.operations"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "de.kalnbach.operations"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0-fieldtest"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".fieldtest"
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {}
